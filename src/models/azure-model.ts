2b79c6f51744dc925ea767b5e58dbfa370ca8d0b
kwartengj <kwartengj@yahoo.co.uk>
2024-10-04 14:19:33 +0100

    change to the meta details for insance id

--- src/models/azure-model.ts
+++ src/models/azure-model.ts
@@ -91,7 +91,7 @@ export class AzureAIInferenceModel extends BaseModel {
             {
                 run_type: "llm",
                 name: "ChatAzureAI",
-                metadata: { ls_provider: "azure-ai-inference", ls_model_name: "azure_model", ls_model_type: "chat", ls_temperature: this.temperature, instance_id: this.instanceId || "undefined" },
+                metadata: { ls_provider: "azure-ai-inference", ls_model_name: "azure_model", ls_model_type: "chat", ls_temperature: this.temperature, ls_raginstance: this.instanceId || "undefined" },
             }
         );
 
